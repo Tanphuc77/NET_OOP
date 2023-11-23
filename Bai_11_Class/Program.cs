9a6a2d7fe776313c40abfa3ad581ab9259522fd6
PhanTanPhuc <phuccode77@gmail.com>
2023-11-23 10:39:00 +0700

    Update_Bai_11_Class

--- Bai_11_Class/Program.cs
+++ Bai_11_Class/Program.cs
@@ -5,6 +5,13 @@
         static void Main(string[] args)
         {
             VuKhi vuKhi = new VuKhi();
+            vuKhi.name = "Súng AK";
+            vuKhi.SetDoSatThuong(10);
+            vuKhi.TanCong();
+
+            Console.WriteLine("============================");
+            VuKhi vuKhi1 = new VuKhi("Súng Máy", 20);
+            vuKhi1.TanCong();
         }
     }
 }
--- Bai_11_Class/VuKhi.cs
+++ Bai_11_Class/VuKhi.cs
@@ -2,9 +2,30 @@ namespace Bai_11_Class
 {
     class VuKhi
     {
+        public string name = "Tên Vũ Khí";
+
+        int dosatthuong = 0;
         public VuKhi()
         {
-            Console.Write("Khoi Tao");
+            this.dosatthuong = 1;
+        }
+        public VuKhi(string name, int dosatthuong)
+        {
+            this.name = name;
+            SetDoSatThuong(dosatthuong);
+        }
+        public void SetDoSatThuong(int mucdo)
+        {
+            this.dosatthuong = mucdo;
+        }
+        public void TanCong()
+        {
+            Console.Write(name + ": \t");
+            for (int i = 0; i < dosatthuong; i++)
+            {
+                Console.Write(" * ");
+            }
+            Console.WriteLine();
         }
     }
 }
